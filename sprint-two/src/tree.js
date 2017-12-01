bfe173752cf78f44067de4db40d22e93e57c0ed1
storycoding <youknownuno@gmail.com>
2017-12-01 11:12:09 -0800

    tree.js - completed

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,13 @@
+// using functional shared method
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.addChild = treeMethods.addChild; 
+  newTree.contains = treeMethods.contains;
+
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +15,35 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  
+  var childTree = Tree(value);
+
+  this.children.push(childTree);
 };
+
 
 treeMethods.contains = function(target) {
-};
 
+  if ( this.value === target ) {
+    return true;
+  }
+
+  for (var i = 0; i < this.children.length; i++) {
+
+    var doesItContain = this.children[i].contains(target);
+    
+    if (doesItContain === true) {
+      return true;
+    }
+
+  }
+  
+  return false;   
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  addChild : Oc constant
+  contains : On linear
  */
