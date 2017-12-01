d0633bb14bc8fe0fbcfcb097e49ad30f47b84d26
storycoding <youknownuno@gmail.com>
2017-12-01 15:40:18 -0800

    sets and binary search trees complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,102 @@
 var BinarySearchTree = function(value) {
+  
+  var tree = {};
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  
+  tree.insert = BSTFunctions.insert;
+  tree.contains = BSTFunctions.contains;
+  tree.depthFirstLog = BSTFunctions.depthFirstLog;
+  
+  return tree;
+};  
+
+var BSTFunctions = {};
+
+BSTFunctions.insert = function(insertee) {
+  if (this.value === insertee) {
+
+    return;
+  }
+
+  if ( insertee < this.value ) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(insertee);
+
+    } else { 
+      this.left.insert(insertee);
+    }
+  }
+
+  if ( insertee > this.value ) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(insertee);
+
+    } else {
+      this.right.insert(insertee);
+    }
+  }
+};
+
+BSTFunctions.contains = function(containee) {
+  if (this.value === containee) {
+
+    return true;
+  }
+
+  if ( containee < this.value ) {
+    if (this.left === null) {
+
+      return false;
+
+    } else { 
+      var doesContain = this.left.contains(containee);
+      if (doesContain === true) {
+
+        return true;
+      }
+    }
+  }
+
+  if ( containee > this.value ) {
+    if (this.right === null) {
+
+      return false;
+
+    } else {
+      var doesContain = this.right.contains(containee);
+      if (doesContain === true) {
+
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+BSTFunctions.depthFirstLog = function(iteratee) {
+  // runs iteratee on every value of our binary tree
+  iteratee(this.value);
+
+  // check if there are children
+  if (this.left !== null) {
+    this.left.depthFirstLog(iteratee);
+  }
+
+  if (this.right !== null) {
+    this.right.depthFirstLog(iteratee);
+
+  } else {
+    return;
+  }
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * insert: O(log n) logarithmic
+ * contains: O(log n) logarithmic
+ * depthFirstLog: O(log n) logarithmic
  */
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,37 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * add: O(c) - constant
+ * contains: O(n) - linear
+ * remove: O(n) - linear
  */
+
