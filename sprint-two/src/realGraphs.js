60ecd407132d1d5d9186371d59dbf5c7a2a74acd
storycoding <youknownuno@gmail.com>
2017-12-02 11:38:51 -0800

    nuno developed an alternative graph for fun

--- sprint-two/src/realGraphs.js
+++ sprint-two/src/realGraphs.js
@@ -1,106 +1,112 @@
+//just for the example, the nodes wouldn't actually be assigned by variables
+var graphNode1 = {
+  value: 'valueOfgraphNode1',
+  edges: {
+    'valueOfgraphNode2': graphNode2
+  }
+};
+
+var graphNode2 = {
+  value: 'valueOfgraphNode1',
+  edges: {
+    'valueOfgraphNode1': graphNode1
+  }
+};
 
 
-// Instantiate a new graph
+// Oc
 var Graph = function(value) {
   this.value = value;
-  this.edges = [];
+  this.edges = {};
 };
 
-// Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(value) { // it used to have node as input
-  var node = new Graph(value);
-  this.edges.push(node);  
+//danger of duplication, should add a hashing function for the key assignment
+Graph.prototype.addNode = function(value) {
+
+  this.edges[value] = new Graph(value);
+
+  //creating the two way access
+  this.edges[value].edges[this.value] = this; 
 };
 
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
-  for (var i = 0; i < this.edges.length; i++) {
-    if (this.edges[i] === node) {
-      return true;
-    }
-  }
+
+// O(c)
+Graph.prototype.addEdge = function(node) {
   
-  return false;
+  this.edges[node.value] = node;
+
+  node.edges[this.value] = this;
+
 };
 
-// Removes a node from the graph.
+
+// O(c)
+Graph.prototype.removeEdge = function(node) {
+  delete this.edges[node.value];
+  delete node.edges[this.value];
+};
+
+
+
+// O(n)
 Graph.prototype.removeNode = function(node) {
-  for (var i = 0; i < this.edges.length; i++) {
-    if (this.edges[i] === node) {
-      this.edges.splice(i, 1);
-    }
+  for (var edge in this.edges) {
+    delete edge[this.value];
   }
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
 
-  //look for the fromNode in the graph
+//O(c)
+var graphRoot = new Graph('element');
 
-  for (var i = 0; i < fromNode.edges.length; i++) {
-    
-    if (fromNode.edges[i] === toNode) {
-      return true;
-    }
-   
+
+// O(c)
+Graph.prototype.hasEdge = function(node) {
+
+  if (this[node.value] !== undefined) {
+    return true;
   }
 
   return false;
 };
 
-// Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
-  
-  fromNode.edges.push(toNode);
 
-};
+// could lead to infinite loop! (if there are circular edges)
+Graph.prototype.contains = function(value) {
 
-// Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
+  for (var edge in this.edges) {
 
-  for (var i = 0; i < fromNode.edges.length; i++) {
-    
-    if (fromNode.edges[i] === toNode) {
-      fromNode.edges.splice(i, 1);
-
-      for (var i = 0; i < toNode.edges.length; i++) {
-
-        if (toNode.edges[i] === fromNode) {
-          toNode.edges.splice(i, 1); 
-        }    
-
-      }
-   
+    if (edge.value === value) {
+      return true;
     }
+
+    if (edge.edges.length = 1) {
+      return false;
+
+    } else {
+      return edge.contains(value); // here's the tricky part
+    }
+
   }
 
+  return false;
 };
 
-// Pass in a callback which will be executed on each node of the graph.
+
+// could lead to infinite loop! (if there are circular edges)
 Graph.prototype.forEachNode = function(cb) {
+  
+  cb(this.value);
+
+  if (this.edges.length = 1) { //needs testing
+      return;  
+
+  for (var edge in this.edges) { // here's the tricky part
+    cb(edge.value);
+  }
+
+  
 };
 
-
-
-/*
-
-var garphield = new Graph();
-
-garphield = {
-  //prototypal funcs
-  addNode(node),
-  contains(node),
-  removeNode(node),
-  hasEdge(fromNode, toNode),
-  addEdge(fromNode, toNode),
-  removeEdge(fromNode, toNode),
-  forEachNode(callback)
-}
-
-*/
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
+// forEachNode and contains can lead to infinity cycles
+// using memoization of the callbacks could potentially fix this
