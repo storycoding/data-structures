935ffacf59700d16ec3ecd7fe47602bd8e23a39b
storycoding <youknownuno@gmail.com>
2017-12-02 11:05:21 -0800

    graph done. only refactoring of sets left

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,16 +1,119 @@
-
-
-// Instantiate a new graph
+//PseudoClasses
 var Graph = function() {
-  //this.value = value;
   this.edges = [];
-  this.nodes = {};
+  this.nodes = [];
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) { // it used to have node as input
-  this.nodes[node] = new Graph;  
+Graph.prototype.addNode = function(node) {
+  this.nodes.push(node);
 };
+
+// Return a boolean value indicating if the value passed to contains is represented in the graph.
+Graph.prototype.contains = function(node) {
+  return this.nodes.includes(node);
+};
+
+// Removes a node from the graph.
+Graph.prototype.removeNode = function(node) {
+  nodeList = this.nodes;
+  edgeList = this.edges;
+
+  for (var i = 0; i < edgeList.length; i++) {
+    if (edgeList[i].includes(node)) {
+      edgeList.splice(i, 1);
+    }
+  }
+
+  for (var i = 0; i < nodeList.length; i++) {
+    if (nodeList[i] === node) {
+      nodeList.splice(i, 1);
+    }
+  }
+  
+};
+
+// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var edgeList = this.edges;
+  //loops through the edges to find a matching fromNode
+  for (var i = 0; i < edgeList.length; i++) {
+    if (edgeList[i][0] === fromNode) {
+      //if fromNode is found, check its paired node against toNode
+      if (edgeList[i][1] === toNode) {
+        return true;
+      }
+      //checks the same way as above but reversing the node order
+    } else if (edgeList[i][0] === toNode) {
+      if (edgeList[i][1] === fromNode) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+// Connects two nodes in a graph by adding an edge between them.
+Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges.push([fromNode, toNode]);
+};
+
+// Remove an edge between any two specified (by value) nodes.
+Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var edgeList = this.edges;
+  //loops through the edges to find a matching fromNode
+  for (var i = 0; i < edgeList.length; i++) {
+    if (edgeList[i][0] === fromNode) {
+      //if fromNode is found, check its paired node against toNode
+      if (edgeList[i][1] === toNode) {
+        edgeList.splice(i, 1);
+      }
+      //checks the same way as above but reversing the node order
+    } else if (edgeList[i][0] === toNode) {
+      if (edgeList[i][1] === fromNode) {
+        edgeList.splice(i, 1);
+      }
+    }
+  }
+};
+
+// Pass in a callback which will be executed on each node of the graph.
+Graph.prototype.forEachNode = function(cb) {
+  var nodeList = this.nodes;
+  for (var i = 0; i < nodeList.length; i++) {
+    cb(nodeList[i]);
+  }
+};
+
+
+/*
+
+//PseudoClasses
+var Graph = function() {
+  this.edges = [];
+  this.nodes = [];
+};
+
+// the nodes are just values, not objects
+var Node = function(value) {
+  this.value = value;
+};
+
+var Edge = function(fromNode, toNode) {
+  this.fromNode = fromNode;
+  this.toNode = toNode;
+};
+
+
+//Functions
+
+// Add a node to the graph, passing in the node's value.
+Graph.prototype.addNode = function(value) { // it used to have node as input
+  this.nodes[Node] = new Node(value);  
+};
+
+
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
@@ -44,8 +147,7 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
   
-  fromNode.edges.push(toNode);
-  toNode.edges.push(fromNode);
+  this.edges.push(new Edge(fromNode, toNode));
 
 };
 
